3fa13d7f05e279dc9aeb1dff5bda5c7a9c4abf83
Vezona <vezona5tv@gmail.com>
2021-07-21 23:20:36 +0800

    完成加入購物車

--- src/components/frontNav.vue
+++ src/components/frontNav.vue
@@ -1,72 +1,112 @@
 <template>
-    <!-- <div id="nav"> -->
-    <nav class="navbar navbar-expand-md navbar-light bg-light px-3 mb-3">
-      <!-- <a class="navbar-brand" href="#">首頁</a> -->
-      <router-link to="/indexfront" class="nav-link">首頁</router-link>
+  <!-- <div id="nav"> -->
+  <nav class="navbar navbar-expand-md navbar-light bg-light px-3 mb-3">
+    <!-- <a class="navbar-brand" href="#">首頁</a> -->
+    <router-link to="/indexfront" class="nav-link">首頁</router-link>
 
-        <!-- 購物車跟登入icon -->
-      <ul class="navbar-nav navbar-icons flex-row">
-          <li class="me-3">
-            <span class="material-icons">
-                <router-link to="/dashboardF/login" class="nav-link member_login">perm_identity</router-link>
-            </span>
-          </li>
-          <li class="me-3 position-relative">
-            <span class="material-icons">
-                <router-link to="/dashboardF/cart" class="nav-link">
-                shopping_cart
-                </router-link>
-                <span class="badge bg-danger position-absolute"
-                v-if="cartItem > 0">{{cartItem}}</span>
-            </span>
-          </li>
-          <li class="me-3">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-            aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-          </li>
+    <!-- 購物車跟登入icon -->
+    <ul class="navbar-nav navbar-icons flex-row">
+      <li class="me-3">
+        <span class="material-icons">
+          <router-link to="/dashboardF/login" class="nav-link member_login"
+            >perm_identity</router-link
+          >
+        </span>
+      </li>
+      <li class="me-3 position-relative">
+        <span class="material-icons">
+          <router-link to="/dashboardF/cart" class="nav-link">
+            shopping_cart
+          </router-link>
+          <span class="badge bg-danger position-absolute" v-if="cartItem > 0">{{
+            cartItem
+          }}</span>
+        </span>
+      </li>
+      <li class="me-3">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </li>
+    </ul>
+
+    <!-- 收闔列表 -->
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav w-100 justify-content-around">
+        <li class="nav-item">
+          <router-link
+            to="/dashboardF/about"
+            class="nav-link"
+            :class="{ active: activeItem === 1 }"
+            @click="addActive(1)"
+            >經營理念</router-link
+          >
+        </li>
+        <li class="nav-item">
+          <router-link
+            to="/dashboardF/newin"
+            class="nav-link"
+            :class="{ active: activeItem === 2 }"
+            @click="addActive(2)"
+            >本季新品</router-link
+          >
+        </li>
+        <li class="nav-item">
+          <router-link
+            to="/dashboardF/ProductsList"
+            class="nav-link"
+            :class="{ active: activeItem === 3 }"
+            @click="addActive(3)"
+            >系列產品</router-link
+          >
+        </li>
+        <li class="nav-item">
+          <router-link
+            to="/dashboardF/onsale"
+            class="nav-link"
+            :class="{ active: activeItem === 4 }"
+            @click="addActive(4)"
+            >限時特價</router-link
+          >
+        </li>
+        <li class="nav-item">
+          <router-link
+            to="/dashboardF/decorateIdea"
+            class="nav-link"
+            :class="{ active: activeItem === 5 }"
+            @click="addActive(5)"
+            >居家佈置靈感</router-link
+          >
+        </li>
+        <li class="nav-item dropdown">
+          <a
+            class="nav-link dropdown-toggle"
+            href="#"
+            id="navbarDropdown"
+            role="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            各式問題
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider" /></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
       </ul>
-
-      <!-- 收闔列表 -->
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav w-100 justify-content-around">
-          <li class="nav-item">
-            <router-link to="/dashboardF/about" class="nav-link"
-            :class="{'active': activeItem === 1}" @click="addActive(1)">經營理念</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/dashboardF/newin" class="nav-link"
-            :class="{'active': activeItem === 2}" @click="addActive(2)">本季新品</router-link>
-          </li>
-          <li class="nav-item ">
-            <router-link to="/dashboardF/ProductsList" class="nav-link"
-            :class="{'active': activeItem === 3}" @click="addActive(3)">系列產品</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/dashboardF/onsale" class="nav-link"
-            :class="{'active': activeItem === 4}" @click="addActive(4)">限時特價</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/dashboardF/decorateIdea" class="nav-link"
-            :class="{'active': activeItem === 5}" @click="addActive(5)">居家佈置靈感</router-link>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              各式問題
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><hr class="dropdown-divider"></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-
-    </nav>
+    </div>
+  </nav>
   <!-- </div> -->
 </template>
 
@@ -74,44 +114,53 @@
 export default {
   data () {
     return {
-      cartItem: 1,
+      cartItem: 0,
       activeItem: 1
     }
+  },
+  mounted () {
+    this.$http
+      .get(`${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/cart`)
+      .then((res) => {
+        // console.log(res)
+        // console.log(res.data.data.carts.length)
+        this.cartItem = res.data.data.carts.length
+      })
+      .catch((err) => {
+        alert(err)
+      })
   },
   methods: {
     addActive (i) {
       this.activeItem = i
     }
   }
-
 }
 </script>
 
 <style lang="scss" scoped>
-.navbar{
+.navbar {
   background-color: $mainColor !important;
-   /* #214c6f; */
+  /* #214c6f; */
   a:not(.dropdown-item) {
-    color:white !important
+    color: white !important;
   }
 }
 
-.nav-link.active{
+.nav-link.active {
   font-weight: bold;
 }
 
-.material-icons .badge{
-  font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+.material-icons .badge {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
   top: -5px;
   right: -10px;
-  font-size:12px
+  font-size: 12px;
 }
 
 @media (min-width: 768px) {
-  .navbar-icons{
-    order:2
+  .navbar-icons {
+    order: 2;
   }
-
 }
-
 </style>
--- src/views/front/Product.vue
+++ src/views/front/Product.vue
@@ -1,64 +1,108 @@
 <template>
-    <div class="wrap">
-      <loading v-if="loading"></loading>
-      <div class="container mt-4" v-else>
-        <breadCrumb class="breadcrumbWrap mb-5" :productTitle=product.title></breadCrumb>
-        <div class="product-Info mb-5 d-flex flex-wrap justify-content-center">
-         <div class="col-10 col-sm-6">
-            <div class="showProductImg">
-              <img :src="previewImage" alt="">
-               <!-- swiper -->
-                <swiper
-                  class="pt-2 pb-4"
-                  :slides-per-view="3"
-                  :space-between="10" :mousewheel='mousewheel'
-                  navigation :pagination="{ clickable: true }">
-                  <swiper-slide v-for="(i, index) in product.imagesUrl" :key="i.index">
-                     <img
-                     :class="{'imgBorder': previewImage === i }"
-                     :src="i" alt="" @click="showImage(i, index)">
-                  </swiper-slide>
-                </swiper>
-          </div>
-         </div>
-
-          <div class="col-10 col-sm-6 px-3 d-flex flex-column justify-content-between">
-            <div class="info">
-             <h4> {{product.title}}</h4>
-             <p>NT$ {{product.price}}</p>
-             <p>商品數量 {{product.num}}</p>
-            </div>
-            <div>
-              <p>{{warning}}</p>
-              <div class="input-group mb-3">
-                <button class="btn btn-outline-secondary" type="button" @click="minus">-</button>
-                <input type="text" class="form-control text-center" :value="number">
-                <button class="btn btn-outline-secondary" type="button" @click="add">+</button>
-              </div>
-              <button class="btn btn-secondary w-100" type="button">加入購物車</button>
-            </div>
-
+  <div class="wrap">
+    <loading v-if="loading"></loading>
+    <div class="container mt-4" v-else>
+      <breadCrumb
+        class="breadcrumbWrap mb-5"
+        :productTitle="product.title"
+      ></breadCrumb>
+      <div class="product-Info mb-5 d-flex flex-wrap justify-content-center">
+        <div class="col-10 col-sm-6">
+          <div class="showProductImg">
+            <img :src="previewImage" alt="" />
+            <!-- swiper -->
+            <swiper
+              class="pt-2 pb-4"
+              :slides-per-view="3"
+              :space-between="10"
+              :mousewheel="mousewheel"
+              navigation
+              :pagination="{ clickable: true }"
+            >
+              <swiper-slide
+                v-for="(i, index) in product.imagesUrl"
+                :key="i.index"
+              >
+                <img
+                  :class="{ imgBorder: previewImage === i }"
+                  :src="i"
+                  alt=""
+                  @click="showImage(i, index)"
+                />
+              </swiper-slide>
+            </swiper>
           </div>
         </div>
-        <!-- 商品描述tab -->
-        <div class="tabWrap">
-          <ul class="nav nav-tabs text-center">
-            <li class="nav-item col-6" v-for="(i, index) in tabs" :key="i.index">
-              <p class="tab" :class="{'active':tabIndex === index}" @click="showtab(index)">{{i}} {{index}}</p>
-            </li>
-          </ul>
-          <div class="tabContent p-3 ">
-            <div v-if="tabIndex===0">
-               <p >{{product.description}}</p>
+
+        <div
+          class="
+            col-10 col-sm-6
+            px-3
+            d-flex
+            flex-column
+            justify-content-between
+          "
+        >
+          <div class="info">
+            <h4>{{ product.title }}</h4>
+            <p>NT$ {{ product.price }}</p>
+            <p>商品數量 {{ product.num }}</p>
+          </div>
+          <div>
+            <p>{{ warning }}</p>
+            <div class="input-group mb-3">
+              <button
+                class="btn btn-outline-secondary"
+                type="button"
+                @click="minus"
+              >
+                -
+              </button>
+              <input
+                type="text"
+                class="form-control text-center"
+                :value="number"
+              />
+              <button
+                class="btn btn-outline-secondary"
+                type="button"
+                @click="add"
+              >
+                +
+              </button>
             </div>
-            <div v-else>
-              退換貨
-              購物
-            </div>
+            <button
+              class="btn btn-secondary w-100"
+              type="button"
+              @click="addToCart"
+            >
+              加入購物車
+            </button>
           </div>
         </div>
       </div>
+      <!-- 商品描述tab -->
+      <div class="tabWrap">
+        <ul class="nav nav-tabs text-center">
+          <li class="nav-item col-6" v-for="(i, index) in tabs" :key="i.index">
+            <p
+              class="tab"
+              :class="{ active: tabIndex === index }"
+              @click="showtab(index)"
+            >
+              {{ i }} {{ index }}
+            </p>
+          </li>
+        </ul>
+        <div class="tabContent p-3">
+          <div v-if="tabIndex === 0">
+            <p>{{ product.description }}</p>
+          </div>
+          <div v-else>退換貨 購物</div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -102,21 +146,20 @@ export default {
     // axios方法連api取資料
     const productId = this.$route.params.id
     const url = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}`
-    this.$http.get(`${url}/product/${productId}`)
-      .then(res => {
-        // console.log(res)
-        if (res.data.success) {
-          this.product = res.data.product
-          // console.log(this.product)
-          this.loading = false
+    this.$http.get(`${url}/product/${productId}`).then((res) => {
+      // console.log(res)
+      if (res.data.success) {
+        this.product = res.data.product
+        // console.log(this.product)
+        this.loading = false
 
-          // 圖庫塞入首圖
-          this.product.imagesUrl.unshift(res.data.product.imageUrl)
-          this.previewImage = res.data.product.imageUrl
-        } else {
-          alert(res.data.message)
-        }
-      })
+        // 圖庫塞入首圖
+        this.product.imagesUrl.unshift(res.data.product.imageUrl)
+        this.previewImage = res.data.product.imageUrl
+      } else {
+        alert(res.data.message)
+      }
+    })
   },
   methods: {
     showImage (imgUrl, index) {
@@ -138,6 +181,26 @@ export default {
         this.number = 1
       }
     },
+    addToCart () {
+      // ${process.env.VUE_APP_API}
+      // /api/:api_path/cart
+      const product = {
+        product_id: this.product.id,
+        qty: this.number
+      }
+      this.$http
+        .post(
+          `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/cart`,
+          { data: product }
+        )
+        .then((res) => {
+          console.log(res)
+          alert(res.data.message)
+        })
+        .catch((err) => {
+          alert(err)
+        })
+    },
     showtab (index) {
       this.tabIndex = index
     }
@@ -146,59 +209,59 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.wrap{
+.wrap {
   min-height: 500px;
 }
-.breadcrumbWrap{
+.breadcrumbWrap {
   background-color: #f8f9fa;
 }
-.showProductImg img{
-    height: 25vw;
+.showProductImg img {
+  height: 25vw;
 }
-.swiper-slide img{
+.swiper-slide img {
   height: 10vw;
   cursor: pointer;
 }
 
 // 輪播點點
 .swiper-container:deep(.swiper-button-prev),
-.swiper-container:deep(.swiper-button-next){
+.swiper-container:deep(.swiper-button-next) {
   display: none;
 }
 
-:deep(.swiper-pagination){
+:deep(.swiper-pagination) {
   bottom: 0;
 }
 
 :deep(.swiper-pagination-bullet) {
-    width: 20px;
-    height: 3px;
-    display: inline-block;
-    border-radius: 0;
-    background: #000;
-    opacity: 0.2;
+  width: 20px;
+  height: 3px;
+  display: inline-block;
+  border-radius: 0;
+  background: #000;
+  opacity: 0.2;
 }
 
-:deep(.swiper-pagination-bullet-active){
+:deep(.swiper-pagination-bullet-active) {
   background: #2b6d7e;
-    opacity: 1;
+  opacity: 1;
 }
 
 //
-.imgBorder{
-  border: 4px solid #2b6d7e
+.imgBorder {
+  border: 4px solid #2b6d7e;
 }
 
-.form-control{
-   border: 1px solid #2b6d7e
+.form-control {
+  border: 1px solid #2b6d7e;
 }
 
 // Tabs
-.tabWrap{
+.tabWrap {
   cursor: pointer;
 }
 
-.tab{
+.tab {
   margin-bottom: -1px;
   background: none;
   border: 1px solid transparent;
@@ -207,36 +270,35 @@ export default {
   display: block;
   padding: 0.5rem 1rem;
   text-decoration: none;
-  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out, border-color 0.15s ease-in-out;
+  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+    border-color 0.15s ease-in-out;
 }
 
-.tab:hover{
-  border: 1px solid rgba(43,109,126,0.2);
+.tab:hover {
+  border: 1px solid rgba(43, 109, 126, 0.2);
   // background-color: rgba(43,109,126,0.2);
   color: black;
 }
 
-.tab.active{
+.tab.active {
   border-bottom: 4px solid #2b6d7e;
   // background-color: #2b6d7e;
   color: #2b6d7e;
   font-weight: bold;
 }
 
-.tabContent{
+.tabContent {
   height: 400px;
-  background-color: rgba(43,109,126,0.1);
+  background-color: rgba(43, 109, 126, 0.1);
 }
 
 @media (max-width: 576px) {
-  .showProductImg img{
+  .showProductImg img {
     height: 50vw;
   }
-  .swiper-slide img{
+  .swiper-slide img {
     height: 20vw;
     cursor: pointer;
   }
-
 }
-
 </style>
